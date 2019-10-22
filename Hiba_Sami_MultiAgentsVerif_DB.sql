9509c5a37d82675f6a07d7ce55dca2d3c9eaa0d2
SAMI <samikobbi10@gmail.com>
2019-10-22 12:50:20 +0100

    test

--- Hiba_Sami_MultiAgentsVerif_DB.sql
+++ Hiba_Sami_MultiAgentsVerif_DB.sql
@@ -2,4 +2,28 @@ select agent_id,agent_dest_id,liking_value from agent as ag inner join agent_lik
 on ag.id = ag_liking_map.agent_id and ag_liking.id = ag_liking_map.liking_toward_agent_id;
 			
             
-select 
+select * from OCEAN where agent_id=2;
+
+select goal_info.agent_id,goal_name.name, goal_info.weight,ocean.conscientiousness from goal_info inner join goal_name on goal_name.id=goal_info.goal_name_id
+inner join ocean on ocean.agent_id=goal_info.agent_id
+order by agent_id;
+
+select goal_name.name,event_name.name, event_degree,confirmed,prospected,likelihood from event_name inner join goal_name on goal_name.id=event_name.goal_name_id;
+
+select * from action;
+select * from occ where occ.id=114;
+
+select * from occ_sorry_for;
+select * from occs_towards_agent;
+
+select event_info.agent_id,goal_name.name,event_name.name,
+event_intensity_level,event_reaction.reaction_name,
+ocean.neuroticism, ocean.conscientiousness
+ from event_info inner join event_name on event_name.id=event_info.event_name_id 
+inner join goal_name on goal_name.id = event_name.goal_name_id
+inner join event_reaction on event_reaction.event_info_id=event_info.id
+inner join ocean on event_info.agent_id=ocean.agent_id
+order by agent_id ;
+
+
+select * from occ;
